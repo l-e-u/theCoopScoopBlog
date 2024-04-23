f0d981143a13c4551a33af7d88acba2fb41a8c6b
Elihu Rodriguez <elihu.rodriguez@outlook.com>
2024-04-23 07:46:16 -0700

    Added Google Analytics and updated site info in Astro Config.

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,17 +5,17 @@ import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  markdown: {
-    drafts: true,
-    shikiConfig: {
-      theme: "css-variables"
-    }
-  },
-  shikiConfig: {
-    wrap: true,
-    skipInline: false,
-    drafts: true
-  },
-  site: 'https://lexingtonthemes.com',
-  integrations: [tailwind(), sitemap(), mdx()]
+   markdown: {
+      drafts: true,
+      shikiConfig: {
+         theme: "css-variables"
+      }
+   },
+   shikiConfig: {
+      wrap: true,
+      skipInline: false,
+      drafts: true
+   },
+   site: 'https://thecoopscoopblog.onrender.com',
+   integrations: [tailwind(), sitemap(), mdx()]
 });
